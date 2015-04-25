eb446f7ddc867711212d0873f691a176fb8fb893
Baahoot <jeffryjsolinger@yahoo.com>
2015-04-25 00:13:51 -0500

    Create als_special.php

---
+++ als_special.php
@@ -0,0 +1,18 @@
+<div align="center"><img src="images/ALS-Special.png" /></div>
+<div style="color: #FFFFFF; font-weight: bold; font-variant: small-caps; text-align: left;">
+PsychoWars is having a special ALS Event! <br />
+The event is to do the ALS Ice Bucket Challenge! <br />
+</div>
+<div style="color: green; font-weight: bold; border-bottom: 1px dotted green; text-align: left">Requirements: </div>
+<div style="color: #FF8000; font-weight: bold; font-style: italic; text-align: left;">
+&raquo; Take a video of you doing the ALS Ice Bucket Challenge <br />
+&raquo; Say you're doing the ALS Ice Bucket Challenge and mention PsychoWars <br />
+&raquo; Post the video and tag me in the video <span onclick="window.open('http://www.facebook.com/Bad.Karma12323','_NEW')">(Add Me Here)</span> <br />
+</div>
+<div style="color: green; font-weight: bold; border-bottom: 1px dotted green; text-align: left;">Rewards: </div>
+<div style="color: #FF8000; font-weight: bold; font-style: italic; text-align: left;">
+&raquo; 150 Boss Points! <br />
+&raquo; 50,000 Energy! <br />
+&raquo; 200 Stamina! <br />
+</div>
+
