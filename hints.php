db4de009af2e85f96f2ee04552727c2867e4ee50
Baahoot <jeffryjsolinger@yahoo.com>
2015-04-25 15:09:48 -0500

    Create hints.php

---
+++ hints.php
@@ -0,0 +1,31 @@
+<?php session_start() ?>
+<?php require 'connect.php' ?>
+<!-- Hints/Tips -->
+<div align="center"><img src="images/Game-Guide.png" /></div>
+<div align="center">
+<div id="TipName">Change Image: </div>
+<div id="TipDesc">&#8226; Go To My Mobster, Then Settings To Change Your Image!</div>
+<div id="TipName">Main: </div> 
+<div id="TipDesc">&#8226; Send Broadcasts Out To Fellow Mobsters!</div>
+<div id="TipName">Missions: </div> 
+<div id="TipDesc">&#8226; Master Missions To Recieve Rewards!</div>
+<div id="TipName">Fighting: </div> 
+<div id="TipDesc">&#8226; Health Plays A Big Role In Fighting!</div>
+<div id="TipName">Estate: </div> 
+<div id="TipDesc">&#8226; Bank Your Cash & Purchase Properties!</div>
+<div id="TipName">The Boss: </div> 
+<div id="TipDesc">&#8226; Visit <i>The Boss</i> For Offers!</div>
+<div id="TipName">Hitlist: </div> 
+<div id="TipDesc">&#8226; Kill Players To Recieve The Players Bounty!</div>
+<div id="TipName">Hospital: </div> 
+<div id="TipDesc">&#8226; Heal Your Mobster For A Fee!</div>
+<div id="TipName">Armory: </div> 
+<div id="TipDesc">&#8226; Purchase Weapons To Fight With!</div>
+<div id="TipName">My Mob: </div> 
+<div id="TipDesc">&#8226; Manage Your Mob & invites!</div>
+<div id="TipName">My Mobster: </div>
+<div id="TipDesc">&#8226; Manage Your Mobster's Settings!</div>
+<div id="TipName">Top Players: </div>
+<div id="TipDesc">&#8226; The Top Accounts In Different Categories!</div>
+</div>
+<!--  Hints/Tips End -->
