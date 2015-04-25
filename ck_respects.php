60437b72074acc7f4022e0f6bdeb2c04108241e6
Baahoot <jeffryjsolinger@yahoo.com>
2015-04-25 14:23:55 -0500

    Create ck_respects.php

---
+++ ck_respects.php
@@ -0,0 +1,4 @@
+<div style="color: #FFFFFF;">
+Respects to CK. I've been mobbing with him for years and he's always been a good friend in game and off game. I've always respected him 100% and always will. I've always backed him and will back him through these hard times that have occured. I hope for him a speedy recovery and to be back, healthy and mobbing again. Love ya man.
+</div>
+<img src="https://fbcdn-sphotos-e-a.akamaihd.net/hphotos-ak-xfp1/v/t1.0-9/10428486_10204366819394573_1009719898760886810_n.jpg?oh=946fe0c3124b1ad02783bc2b0f692d17&oe=5480681B&__gda__=1417486206_6b1f326f7607a08407350e40a4d8f027" width="560" />
