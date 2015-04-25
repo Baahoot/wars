efc8308d99e46aa43396887127eeb76406dbffc8
Baahoot <jeffryjsolinger@yahoo.com>
2015-04-25 18:39:04 -0500

    Create video-test.php

---
+++ video-test.php
@@ -0,0 +1,8 @@
+<body>
+<video width="320" height="240" controls autoplay>
+  <source src="images/Game-Weapon-Adding.mp4" type="video/mp4">
+  <object data="images/Game-Weapon-Adding.mp4" width="320" height="240">
+    <embed width="600" height="400" src="images/Game-Weapon-Adding.mp4">
+  </object>
+</video>
+</body>
