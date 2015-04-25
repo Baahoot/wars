4c7464a7593d6c1ab57a647fb2b746c7f1ff4f4f
Baahoot <jeffryjsolinger@yahoo.com>
2015-04-25 14:36:08 -0500

    Create daily_pick.php

---
+++ daily_pick.php
@@ -0,0 +1,8 @@
+<?php session_start() ?>
+<?php require 'connect.php' ?>
+<div id="PickBlock">Click Here</div>
+<div id="PickBlock">Click Here</div>
+<div id="PickBlock">Click Here</div>
+<div id="PickBlock">Click Here</div>
+<div id="PickBlock">Click Here</div>
+<div id="PickBlock">Click Here</div>
